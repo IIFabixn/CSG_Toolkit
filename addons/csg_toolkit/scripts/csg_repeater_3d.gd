02fc8796769c67cd75c492d92aa86e9f9d3d0fbd
Fabian Becker <f.becker33@outlook.de>
2024-06-20 07:50:52 +0200

    convert repeater to vec3 and added vec3 spacing

--- addons/csg_toolkit/scripts/csg_repeater_3d.gd
+++ addons/csg_toolkit/scripts/csg_repeater_3d.gd
@@ -3,43 +3,34 @@ class_name CSGRepeater3D extends CSGCombiner3D
 
 var template_node: Node3D
 
-@export_group("Repeat")
-var _repeat_x: int = 0
-@export var repeat_x: int:
+var _repeat: Vector3 = Vector3.ZERO
+@export var repeat: Vector3:
 	get:
-		return _repeat_x
+		return _repeat
 	set(value):
-		_repeat_x = value
-		repeat()
+		_repeat = value
+		repeat_template()
 
-var _repeat_y: int = 0
-@export var repeat_y: int:
+var _spacing: Vector3 = Vector3.ZERO
+@export var spacing: Vector3:
 	get:
-		return _repeat_y
+		return _spacing
 	set(value):
-		_repeat_y = value
-		repeat()
-
-var _repeat_z: int = 0
-@export var repeat_z: int:
-	get:
-		return _repeat_z
-	set(value):
-		_repeat_z = value
-		repeat()
+		_spacing = value
+		repeat_template()
 
 func _enter_tree():
 	if get_child_count() == 1:
 		template_node = get_child(0)
 	# Connect the child_entered_tree signal to update_template_node method
 	child_entered_tree.connect(update_template_node)
-	repeat()
+	repeat_template()
 
 func update_template_node(node: Node):
 	if node is Node3D and get_child_count() == 1:
 		template_node = node
 
-func repeat():
+func repeat_template():
 	print("Repeat")
 	if not template_node:
 		print("No template node found.")
@@ -51,11 +42,11 @@ func repeat():
 			child.queue_free()
 	
 	# Create new repeated instances
-	for x in range(repeat_x + 1):
-		for y in range(repeat_y + 1):
-			for z in range(repeat_z + 1):
+	for x in range(repeat.x + 1):
+		for y in range(repeat.y + 1):
+			for z in range(repeat.z + 1):
 				if x == 0 and y == 0 and z == 0:
 					continue # Skip the original template node position
 				var new_instance = template_node.duplicate()
-				new_instance.translate(Vector3(x, y, z))
+				new_instance.translate(Vector3(x * (spacing.x + 1), y * (spacing.y + 1), z * (spacing.z + 1)))
 				add_child(new_instance)
